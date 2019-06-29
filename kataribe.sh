031a96f8239d4db3c162f55cbb907930521cceb3
fono09 <fono09@users.noreply.github.com>
2019-06-29 10:42:26 +0000

    Update gitignore
    /kataribe.sh

---
+++ kataribe.sh
@@ -0,0 +1,2 @@
+curl -X POST --data-urlencode "payload={\"channel\": \"#kataribe\", \"username\": \"webhookbot\", \"text\": \"これは webhookbot という名のボットから #kataribe に投稿されています。\", \"icon_emoji\": \":ghost:\"}" https://hooks.slack.com/services/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
+
